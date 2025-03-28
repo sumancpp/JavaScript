84098decf266f7db33e1b7b821d3b2f38267d25a
Suman Maity <someone.cpp@proton.me>
2025-03-28 23:21:02 +0530

    Add files via upload

---
+++ Array/arraypart1.js
@@ -0,0 +1,56 @@
+// ******************Array*******************
+
+const myarr = [0, 1, 2, 3, 4];
+// console.log(myarr);
+
+// const heros = ["Suman", "Akash"];
+// console.log(heros);
+
+// const myarr2 = new Array(1, 2, 3);
+// console.log(myarr2);
+
+// myarr2.push(5);
+// console.log(myarr2);
+
+// myarr2.pop();
+// console.log(myarr2);
+
+// myarr2.unshift(9);//Its add the number in the first position of the array.
+// console.log(myarr2);
+
+// myarr2.shift();//Its remove the number from the first position of the array.
+// console.log(myarr2);
+
+// console.log(myarr2.includes(9));//Its give the answer true or false.
+
+// console.log(myarr2.indexOf(2));
+
+// const myArr3 = myarr2.join();
+// console.log(myArr3);
+// console.log(typeof myArr3);
+
+// Slice    Splice
+console.log("A" , myarr);
+
+const myn1 = myarr.slice(1,3);
+console.log(myn1);
+
+console.log("B",myarr);
+const myn2 = myarr.splice(1,3);
+console.log("c",myarr);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
