b3dfd2e958c80a694595aaaa331580d2ee7712ee
Suman Maity <someone.cpp@proton.me>
2025-05-26 23:26:49 +0530

    Update promises.js

--- Promises/promises.js
+++ Promises/promises.js
@@ -83,7 +83,6 @@ async function consumePromiseFive(){
 consumePromiseFive()
 
 
-
 // async function getAllUsers(){
 //     try {
 //         const response = await fetch('https://jsonplaceholder.typicode.com/users')
